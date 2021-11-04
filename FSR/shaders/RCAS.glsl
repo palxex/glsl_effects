b5935903801284d376101f5874208b966cc25837
palxex <ex+home@palx.org>
2021-11-04 15:42:55 +0800

    Initial commit: FidelityFX-FSR ported

---
+++ FSR/shaders/RCAS.glsl
@@ -0,0 +1,169 @@
+#pragma parameter FSR_SHARPENING "FSR RCAS Sharpening Amount (Lower = Sharper)" 0.2 0.0 2.0 0.1
+
+#if defined(VERTEX)
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+
+COMPAT_VARYING vec2 v_texCoord;
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    v_texCoord = TexCoord.xy;
+}
+#elif defined(FRAGMENT)
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+#ifdef PARAMETER_UNIFORM
+uniform COMPAT_PRECISION float FSR_SHARPENING;
+#else
+#define FSR_SHARPENING 0.2
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+COMPAT_VARYING vec2 v_texCoord;
+
+uniform sampler2D tex0;
+
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+
+// This is set at the limit of providing unnatural results for sharpening.
+#define FSR_RCAS_LIMIT (0.25-(1.0/16.0))
+
+#define saturate(x) clamp(x,0,1)
+
+void main() {
+	vec3 err={0,0,0};
+	vec2 sp = floor(v_texCoord * TextureSize);
+	//sp.x*=1.28;
+	//sp.y*=1.707;
+	//err=vec3(sp.x/255.0,sp.y/255.0,0);
+
+	// Algorithm uses minimal 3x3 pixel neighborhood.
+	//    b 
+	//  d e f
+	//    h
+	vec3 b = texelFetch(tex0 , ivec2(sp.x, sp.y - 1), 0).rgb;
+	//#error 97
+	vec3 d = texelFetch(tex0 , ivec2(sp.x, sp.y)    , 0).rgb;
+	vec3 e = texelFetch(tex0 , ivec2(sp.xy)         , 0).rgb;
+	vec3 f = texelFetch(tex0 , ivec2(sp.x + 1, sp.y), 0).rgb;
+	vec3 h = texelFetch(tex0 , ivec2(sp.x, sp.y + 1), 0).rgb;
+	//err = vec3(b);
+	// Rename (32-bit) or regroup (16-bit).
+	float bR = b.r;
+	float bG = b.g;
+	float bB = b.b;
+	float dR = d.r;
+	float dG = d.g;
+	float dB = d.b;
+	float eR = e.r;
+	float eG = e.g;
+	float eB = e.b;
+	float fR = f.r;
+	float fG = f.g;
+	float fB = f.b;
+	float hR = h.r;
+	float hG = h.g;
+	float hB = h.b;
+	//#error 116
+
+	float nz;
+
+	// Luma times 2.
+	float bL = bB * 0.5 + (bR * 0.5 + bG);
+	float dL = dB * 0.5 + (dR * 0.5 + dG);
+	float eL = eB * 0.5 + (eR * 0.5 + eG);
+	float fL = fB * 0.5 + (fR * 0.5 + fG);
+	float hL = hB * 0.5 + (hR * 0.5 + hG);
+
+	// Noise detection.
+	nz = 0.25 * bL + 0.25 * dL + 0.25 * fL + 0.25 * hL - eL;
+	nz = saturate(abs(nz) * rcp(max3(max3(bL, dL, eL), fL, hL) - min3(min3(bL, dL, eL), fL, hL)));
+	nz = -0.5 * nz + 1.0;
+
+	// Min and max of ring.
+	float mn4R = min(min3(bR, dR, fR), hR);
+	float mn4G = min(min3(bG, dG, fG), hG);
+	float mn4B = min(min3(bB, dB, fB), hB);
+	float mx4R = max(max3(bR, dR, fR), hR);
+	float mx4G = max(max3(bG, dG, fG), hG);
+	float mx4B = max(max3(bB, dB, fB), hB);
+	// Immediate constants for peak range.
+	vec2 peakC = { 1.0, -1.0 * 4.0 };
+	// Limiters, these need to be high precision RCPs.
+	float hitMinR = mn4R * rcp(4.0 * mx4R);
+	float hitMinG = mn4G * rcp(4.0 * mx4G);
+	float hitMinB = mn4B * rcp(4.0 * mx4B);
+	float hitMaxR = (peakC.x - mx4R) * rcp(4.0 * mn4R + peakC.y);
+	float hitMaxG = (peakC.x - mx4G) * rcp(4.0 * mn4G + peakC.y);
+	float hitMaxB = (peakC.x - mx4B) * rcp(4.0 * mn4B + peakC.y);
+	float lobeR = max(-hitMinR, hitMaxR);
+	float lobeG = max(-hitMinG, hitMaxG);
+	float lobeB = max(-hitMinB, hitMaxB);
+	float lobe = max(-FSR_RCAS_LIMIT, min(max3(lobeR, lobeG, lobeB), 0)) * exp2(-FSR_SHARPENING);
+
+	// Apply noise removal.
+	lobe *= nz;
+
+	// Resolve, which needs the medium precision rcp approximation to avoid visible tonality changes.
+	float rcpL = rcp(4.0 * lobe + 1.0);
+	vec3 c = {
+		(lobe * bR + lobe * dR + lobe * hR + lobe * fR + eR) * rcpL,
+		(lobe * bG + lobe * dG + lobe * hG + lobe * fG + eG) * rcpL,
+		(lobe * bB + lobe * dB + lobe * hB + lobe * fB + eB) * rcpL
+	};
+
+	FragColor = vec4(c, 1.0f);
+	if( err.r != 0 || err.g != 0 || err.b != 0 )
+		FragColor=vec4(err,1);
+	//FragColor = vec4(COMPAT_TEXTURE(tex0 , v_texCoord.xy).rgb, 1.0f);
+}
+#endif
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo contains glsl effects that compatible with RetroArch/SDLPal, most of them ports from magpie effects. 
+Thanks for contributing:)
